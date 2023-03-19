950547a7a610d03eaa8fc16b60b1d173ccca304f
michelle nderi <nderimichelle@gmail.com>
2023-03-19 22:14:36 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers,nameToFind) {
+   const matchingDrivers = drivers.filter(driver => driver.toLowerCase() == nameToFind.toLowerCase());
+     return matchingDrivers;
+}
+
+function fuzzyMatch(drivers,nameToFind){
+  const matchingDrivers = drivers.filter(function(driver){
+    return driver.startsWith(nameToFind);
+  });
+  return matchingDrivers;
+}
+
+function matchName(drivers,nameToFind){
+  const matchingDrivers= drivers.filter(driver => driver.name === nameToFind);
+  return matchingDrivers;
+}
+
+
